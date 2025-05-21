5c31a0701fee0ac75e57027a7e7f2828d33405be
踏影 <taying.yy@alibaba-inc.com>
2025-05-21 12:32:24 +0800

    chore: modify

--- premake5.lua
+++ premake5.lua
@@ -63,8 +63,3 @@ project "GLFW"
   filter "configurations:Release"
     runtime "Release"
     optimize "on"
-
-  filter "configurations:Dist"
-    runtime "Release"
-    optimize "on"
-    symbols "off"
